e2bfc42a58c78aa0e39936fe8965d97f7b51592a
cristianty1086 <ctinocoy123@gmail.com>
2022-10-24 15:30:16 -0500

    reconocible y con dockerfile

--- src/main/java/com/nttdata/bootcamp/assignement1/clients/ClientsApplication.java
+++ src/main/java/com/nttdata/bootcamp/assignement1/clients/ClientsApplication.java
@@ -3,9 +3,11 @@ package com.nttdata.bootcamp.assignement1.clients;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class ClientsApplication {
 
 	public static void main(String[] args) {
